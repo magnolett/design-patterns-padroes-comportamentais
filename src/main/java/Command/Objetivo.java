d6959657f4be869f4d5f4955e8f62b30082024f2
Marquinhos <marcos.vinicius@assertsistemas.com.br>
2017-11-13 11:08:05 -0200

    first commit

---
+++ src/main/java/Command/Objetivo.java
@@ -0,0 +1,11 @@
+package Command;
+
+public @interface Objetivo {
+//Controlar as chamadas a um determinado componente, modelando
+// cada requisição como um objeto. Permitir que as operações possam ser
+// desfeitas, enfileiradas ou registradas.
+	
+}
+ 
+
+
